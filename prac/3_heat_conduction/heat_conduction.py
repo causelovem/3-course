7c342d6e4bec71cbb014c63ef03a8681e4706970
causelovem <rat.taurus@gmail.com>
2016-10-21 13:55:03 +0300

    competed mashgraph

--- prac/3_heat_conduction/heat_conduction.py
+++ prac/3_heat_conduction/heat_conduction.py
@@ -1,23 +1,36 @@
 import sys
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def explicit_metod(data, num, dist, a, step, fin):
-    for i in xrange(0, fin):
-        new_data = []
-        new_data.extend(data)
-        print(new_data[0])
-        for j in xrange(1, num - 2):
-            tmp = (data[j + 1] - 2 * data[j] + data[j - 1])
-            new_data[j] = step * a * tmp / (dist ** 2) + data[j]
-            print(new_data[j])
-        print (new_data[num - 1])
-        print ("\n")
-        data = new_data
+    for i in range(1, data.shape[0]):
+        data[i][0] = data[i - 1][0]
+        data[i][num - 1] = data[i - 1][num - 1]
+        for j in range(1, data.shape[1] - 1):
+            point = (data[i - 1][j + 1] + data[i - 1][j - 1])
+            point = point - 2 * data[i - 1][j]
+            data[i][j] = step * a * point / (dist ** 2) + data[i - 1][j]
+    return
+
+
+def plot(data, step):
+    plt.figure()
+    plt.ion()
+    for i in range(data.shape[0]):
+        plt.plot(data[i])
+        plt.title('Temperature_changes')
+        plt.grid(True)
+        plt.pause(0.1)
+#        plt.clf()
+
+    plt.show(block=True)
     return
 
 
 if len(sys.argv) != 3:
     print ">Unexpected quantity of arguments, check your comand string.\n"
+    exit(-1)
 
 in_file = open(sys.argv[1])
 
@@ -31,12 +44,13 @@ a = float(tmp[2])
 step = float(tmp[3])
 fin = int(tmp[4])
 
-data = file[1].split()
+tmp = file[1].split()
+data = np.zeros((fin, num))
 
-for i in range(len(data)):
-    data[i] = float(data[i])
+for i in range(data.shape[1]):
+    data[0][i] = float(tmp[i])
 
-if tmp:
-    explicit_metod(data, num, dist, a, step, fin)
+explicit_metod(data, num, dist, a, step, fin)
+plot(data, step)
 
 in_file.close()
