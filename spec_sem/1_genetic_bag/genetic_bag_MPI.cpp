cddd7e38768d697d20fa0f8260f668062a89d072
causelovem <rat.taurus@gmail.com>
2016-10-31 14:18:38 +0300

    MPI genetic_bag

--- spec_sem/1_genetic_bag/genetic_bag_MPI.cpp
+++ spec_sem/1_genetic_bag/genetic_bag_MPI.cpp
@@ -13,13 +13,14 @@
 #define GEN_END 1000
 #define PERS 0.3
 #define MUT_PERS 3
+#define EXCHANGE 10
 
 using namespace std;
 
 struct item
 {
-    double vol;
-    double val;
+    int vol;
+    int val;
 };
 
 struct parents
@@ -31,7 +32,7 @@ struct parents
 struct generation
 {
     uint gen;
-    double gen_fit;
+    int gen_fit;
 };
 
 struct scomp
@@ -42,7 +43,7 @@ struct scomp
     }
 } comp;
 
-void mutation (uint &gen, uint &quan)
+void mutation (uint &gen, const uint quan)
 {
     uint pos = 0, point = 1, tmp_gen = 0;
 
@@ -64,7 +65,7 @@ void mutation (uint &gen, uint &quan)
     return;
 }
 
-int fitness (const generation &individual, const vector< item > &items, const int &volume)
+int fitness (const generation &individual, const vector< item > &items, const int volume)
 {
     int fit = 0, v = 0;
     uint one = 1, tmp = 0, count = 0;
@@ -86,7 +87,7 @@ int fitness (const generation &individual, const vector< item > &items, const in
     return fit;
 }
 
-uint cross (parents par, uint &quan)
+uint cross (parents par, const uint quan)
 {
     uint point = 0, res = 0;
     uint top = 1;
@@ -109,35 +110,18 @@ uint cross (parents par, uint &quan)
     return res;
 }
 
-void selection (const vector< item > &items, vector<generation> &generat, const int &volume)
+void selection (const vector< item > &items, vector<generation> &generat, const int volume, const int to, const int from)
 {
-    /*int nProc, myRank, root = 0, tag = 0;
-    MPI_Status status;
-
-    MPI_Comm_size(MPI_COMM_WORLD, &nProc);*/
-
     vector<parents> parent;
     uint quan = items.size();
 
     uint pair = GEN_BEGIN * PERS;
 
+    int tag1 = 0, tag2 = 0;
+    MPI_Status status;
+
     for (uint i = 0; i < GEN_END; i++)
     {
-        /*MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
-        int count = GEN_BEGIN / nProc;
-        int step = 0;
-        if (myRank == root)
-        {
-            std::vector<generation> tmp(count);
-            for (uint j = step; j < step + count; j++)
-                tmp.push_back(generat[j]);
-            step += count;
-
-            for (uint j = 1; j < nProc; j++)
-                MPI_Send(&tmp, count, generation, j, tag, MPI_COMM_WORLD);
-            
-        }*/
-
         for (uint j = 0; j < GEN_BEGIN; j++)
             generat[j].gen_fit = fitness(generat[j], items, volume);
         sort(generat.begin(), generat.end(), comp);
@@ -161,6 +145,27 @@ void selection (const vector< item > &items, vector<generation> &generat, const 
 
         for (uint j = 0; j < pair; j++)
             generat[j].gen = cross(parent[j], quan);
+
+        if ((i != 0) && ((i % EXCHANGE) == 0))
+        {
+            sort(generat.begin(), generat.end(), comp);
+
+            uint *send = new uint [pair];
+            uint *recv = new uint [pair];
+
+            int size = generat.size();
+            for (uint j = 0; j < pair; j++)
+                send[j] = generat[size - 1 - j].gen;
+
+            MPI_Sendrecv(send, pair, MPI_UNSIGNED, to, tag1, recv, pair, MPI_UNSIGNED,
+                from, tag2, MPI_COMM_WORLD, &status);
+
+            for (uint j = 0; j < pair; j++)
+                generat[j].gen = recv[j];
+
+            delete [] send;
+            delete [] recv;
+        }
     }
 
     for (uint j = 0; j < GEN_BEGIN; j++)
@@ -178,59 +183,149 @@ int main(int argc, char *argv[])
         return -1;
     } 
 
-    ifstream infile(argv[1]);
-    if (infile.is_open() == false)
+    MPI_Init(&argc, &argv);
+
+    int nProc, myRank, root = 0, tag = 0;
+    MPI_Status status;
+
+    MPI_Comm_size(MPI_COMM_WORLD, &nProc);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
+
+    if (myRank == root)
     {
-        cerr << ">Can not open infile with such name." << endl;
-        return -1;
+        ifstream infile(argv[1]);
+        if (infile.is_open() == false)
+        {
+            cerr << ">Can not open infile with such name." << endl;
+            return -1;
+        }
+
+        srand(time(0));
+
+        int volume = 0;
+        int quan = 0;
+        vector< item > items;
+
+        infile >> volume;
+        infile >> quan;
+
+        items.resize(quan);
+
+        for (uint i = 0; i < quan; i++)
+        {
+            infile >> items[i].vol; /*volume*/
+            infile >> items[i].val; /*value*/
+        }
+
+        for (uint i = 1; i < nProc; i++)
+        {
+            int rand_val;
+            rand_val = rand();
+            MPI_Send(&rand_val, 1, MPI_INT, i, tag, MPI_COMM_WORLD);
+        }
+        
+        MPI_Bcast(&volume, 1, MPI_INT, root, MPI_COMM_WORLD);
+        MPI_Bcast(&quan, 1, MPI_INT, root, MPI_COMM_WORLD);
+        int *tmp_vol = new int [quan];
+        int *tmp_val = new int [quan];
+
+        for (uint i = 0; i < quan; i++)
+        {
+            tmp_vol[i] = items[i].vol;
+            tmp_val[i] = items[i].val;
+        }
+
+        MPI_Bcast(tmp_vol, quan, MPI_INT, root, MPI_COMM_WORLD);
+        MPI_Bcast(tmp_val, quan, MPI_INT, root, MPI_COMM_WORLD);
+
+        delete [] tmp_vol;
+        delete [] tmp_val;
+
+        vector<generation> generat;
+
+        generat.resize(GEN_BEGIN);
+        int top = 1;
+        top = (top << quan) - 1;
+
+        for (uint i = 0; i < GEN_BEGIN; i++)
+            generat[i].gen = (int)(rand()) % top + 1;
+
+        selection(items, generat, volume, 1, nProc - 1);
+
+        std::vector<uint> res_vec;
+        res_vec.resize(nProc);
+        res_vec[0] = generat[GEN_BEGIN - 1].gen;
+
+        for (uint i = 1; i < nProc; i++)
+        {
+            uint gen;
+            MPI_Recv(&gen, 1, MPI_UNSIGNED, i, tag, MPI_COMM_WORLD, &status);
+            res_vec[i] = gen;
+        }
+
+        sort(res_vec.begin(), res_vec.end());
+
+        uint one = 1 << (quan - 1), count = quan - 1;
+        //uint tmp = 0, fit = 0, v = 0, res = generat[GEN_BEGIN - 1].gen;
+        uint tmp = 0, fit = 0, v = 0, res = res_vec[nProc - 1];
+
+        cout << endl << ">Best chrom:" << endl;
+        for (int i = quan - 1; i > -1; i--)
+        {
+            tmp = res & one;
+            tmp = tmp >> count;
+            fit +=  tmp * items[quan - 1 - i].val;
+            v += tmp * items[quan - 1 - i].vol;
+            cout << tmp << " ";
+            count--;
+            one = one >> 1;
+        }
+        cout << endl << endl << ">Value = " << fit << endl << ">Volume = " << v << endl;
+
+        infile.close();
+    }
+    else
+    {
+        int rand_val = 0;
+        MPI_Recv(&rand_val, 1, MPI_INT, root, tag, MPI_COMM_WORLD, &status);
+
+        srand(rand_val);
+
+        int quan, volume;
+        MPI_Bcast(&volume, 1, MPI_INT, root, MPI_COMM_WORLD);
+        MPI_Bcast(&quan, 1, MPI_INT, root, MPI_COMM_WORLD);
+
+        int *tmp_vol = new int [quan];
+        int *tmp_val = new int [quan];
+        MPI_Bcast(tmp_vol, quan, MPI_INT, root, MPI_COMM_WORLD);
+        MPI_Bcast(tmp_val, quan, MPI_INT, root, MPI_COMM_WORLD);
+
+        vector< item > items;
+        items.resize(quan);
+
+        for (uint i = 0; i < quan; i++)
+        {
+            items[i].vol = tmp_vol[i]; /*volume*/
+            items[i].val = tmp_val[i]; /*value*/
+        }
+
+        delete [] tmp_vol;
+        delete [] tmp_val;
+
+        vector<generation> generat;
+
+        generat.resize(GEN_BEGIN);
+        int top = 1;
+        top = (top << quan) - 1;
+
+        for (uint i = 0; i < GEN_BEGIN; i++)
+            generat[i].gen = (int)(rand()) % top + 1;
+
+        selection(items, generat, volume, (myRank + 1) % nProc, myRank - 1);
+
+        MPI_Send(&generat[GEN_BEGIN - 1].gen, 1, MPI_UNSIGNED, root, tag, MPI_COMM_WORLD);
     }
 
-    /*MPI_Init(&argc, &argv);*/
-    srand(time(0));
-
-    int volume = 0;
-    int quan = 0;
-    vector< item > items;
-
-    infile >> volume;
-    infile >> quan;
-
-    items.resize(quan);
-
-    for (uint i = 0; i < quan; i++)
-    {
-        infile >> items[i].vol; /*volume*/
-        infile >> items[i].val; /*value*/
-    }
-
-    vector<generation> generat;
-
-    generat.resize(GEN_BEGIN);
-    int top = 1;
-    top = (top << quan) - 1;
-
-    for (uint i = 0; i < GEN_BEGIN; i++)
-        generat[i].gen = (int)(rand()) % top + 1;
-
-    selection(items, generat, volume);
-
-    uint one = 1 << (quan - 1), count = quan - 1;
-    uint tmp = 0, fit = 0, v = 0, res = generat[GEN_BEGIN - 1].gen;
-
-    cout << endl << ">Best chrom:" << endl;
-    for (int i = quan - 1; i > -1; i--)
-    {
-        tmp = res & one;
-        tmp = tmp >> count;
-        fit +=  tmp * items[quan - 1 - i].val;
-        v += tmp * items[quan - 1 - i].vol;
-        cout << tmp << " ";
-        count--;
-        one = one >> 1;
-    }
-    cout << endl << endl << ">Value = " << fit << endl << ">Volume = " << v << endl;
-
-    infile.close();
     MPI_Finalize();
     return 0;
 }
