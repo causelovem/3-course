361850ad5a1e7e8252080e517886c12915ed63b1
causelovem <rat.taurus@gmail.com>
2016-11-14 16:58:15 +0300

    series

---
+++ prac/4_series/fouire_series.py
@@ -0,0 +1,66 @@
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plot(data, a, eps):
+    plt.figure()
+    plt.ion()
+    cords = np.arange(a - eps / 2, a + eps / 2, eps / data.shape[1])
+    for i in range(data.shape[0]):
+        plt.title('Aproximation Fouire')
+        plt.xlim(a - eps / 2, a + eps / 2)
+        plt.ylim(data.min(), data.max())
+        # Asixs
+        plt.plot([a - eps / 2, a + eps / 2], [0, 0], 'k-', lw=1)
+        plt.plot([0, 0], [data.min(), data.max()], 'k-', lw=1)
+        # Func
+        plt.plot(cords, data[0])
+        # Aprox
+        plt.plot(cords, data[i])
+        plt.grid(True)
+        plt.pause(0.5)
+        plt.clf()
+        # Uncomment/comment code-string above to draw different plots
+    plt.show(block=True)
+    return
+
+
+def taylor(data, xcord, a):
+    # function e ^ x
+    # function 2 ^ x
+    # function sin x
+
+    for i in range(2, data.shape[0]):
+        factor = math.factorial(i - 1)
+        # deriv = math.exp(a) / factor
+        # deriv = (2 ** a) * (math.log(2) ** (i - 1)) / factor
+        deriv = math.sin(a + (math.pi * (i - 1)) / 2) / factor
+        for x in range(0, data.shape[1]):
+            derivative[i][x] = derivative[i - 1][x] + deriv * ((xcord[x] - a) ** (i - 1))
+
+
+a = 0.0
+eps = 5.0
+point_num = 100
+der_num = 11
+derivative = np.zeros((der_num, point_num))
+xcord = np.zeros(point_num)
+
+step = a - eps / 2
+
+for x in range(0, point_num):
+    xcord[x] = step
+    step += eps / point_num
+
+for x in range(0, point_num):
+    # derivative[0][x] = math.exp(xcord[x])
+    # derivative[1][x] = math.exp(a)
+    # derivative[0][x] = 2 ** xcord[x]
+    # derivative[1][x] = 2 ** a
+    derivative[0][x] = math.sin(xcord[x])
+    derivative[1][x] = math.sin(a)
+
+taylor(derivative, xcord, a)
+
+plot(derivative, a, eps)
